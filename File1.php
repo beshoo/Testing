50efc823ba16a273daddb01003c15d49b81ee2a4
Bashar Al Masri <beshoo@gmail.com>
2024-01-30 16:06:42 +0300

    s

--- File1.php
+++ File1.php
@@ -2,3 +2,10 @@
 $love = 'bebeb00000333333333333333330000ebebebeb';
 $love = 'beb4444444444444444444444444eb========ebebebeb';
 $sport = str_replace('sss','eeeببببببببببببببببببببببe',$love);
+
+
+$love = 'beb4444444444444444444444444eb========ebebebeb';
+$love = 'beb4444444444444444444444444eb========ebebebeb';
+$sport = str_replace('sss','eeeببببببببببببببببببببببe',$love);
+
+$sport = str_replace('sss','eeeببببببببببببببببببببببe',$love);
